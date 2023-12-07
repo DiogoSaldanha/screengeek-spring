5263718cf29c50f52806d6ad6da6beddcc468f63
DiogoSaldanha <diogossaldanha@hotmail.com>
2023-12-07 18:25:49 -0300

    Update

--- src/main/java/br/com/screengeek/ScreengeekApplication.java
+++ src/main/java/br/com/screengeek/ScreengeekApplication.java
@@ -1,17 +1,10 @@
 package br.com.screengeek;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.screengeek.model.DadosDaSerie;
-import br.com.screengeek.model.DadosDaTemporada;
-import br.com.screengeek.model.DadosDoEpisodio;
-import br.com.screengeek.service.ConsumoApi;
-import br.com.screengeek.service.ConverteDados;
+import br.com.screengeek.main.Main;
 
 //Implements CommandLineRunner para poder puxar o método 'run()', que é basicamente o nosso main().
 @SpringBootApplication
@@ -23,40 +16,8 @@ public class ScreengeekApplication implements CommandLineRunner{
 
 	@Override
 	public void run(String... args) throws Exception {
-		var consumoApi = new ConsumoApi();
-		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=game+of+thrones&apikey=4e7d669c");
-		System.out.println("\n\nTodas as informações do JSON --->  " + json);
-
-		//Consegue converter a API para um objeto instanciado.
-		ConverteDados conversor = new ConverteDados();
-
-		//Fazendo a conversão do Json e atribuindo para uma variável, nesse caso uma classe DadosDaSerie.
-		DadosDaSerie dados = conversor.obterDados(json, DadosDaSerie.class);
-		System.out.println("\n\nInformações retiradas desse JSON pela classe DadosDaSerie --->  " + dados);
-
-		//Antes de utilizar o conversor de novo, vou mudar a variável json, para que a busca do espisódio seja feita corretamente.
-		json = consumoApi.obterDados("https://www.omdbapi.com/?t=game+of+thrones&season=4&episode=2&apikey=4e7d669c");
-		DadosDoEpisodio dadosDoEpisodio = conversor.obterDados(json, DadosDoEpisodio.class);
-		System.out.println("\n\nInformações retiradas do novo JSON pela classe DadosDoEpisodio --->  " + dadosDoEpisodio);
-
-
-		//Lista vai ser usada para armazenar todas as instâncias, para depois imprimir a lista.
-		List<DadosDaTemporada> temporadas = new ArrayList<>();
-		//Mesmo processo, mas o primeiro for serve para criar um objeto apra cada uma das temporadas e armazenar no array 'temporadas'. Para saber quantas vezes iterar (número de temporadas da serie), apenas buscar o atributo temporadas da classe DadosDaSerie instanciada anteriormente. 
-		//A cada iteração, é necessário alterar o link.
-		System.out.println("\n\nInformações de todas as temporadas da série: \n");
-		for (int i = 1; i <= dados.temporadas(); i++) {
-			json = consumoApi.obterDados("https://www.omdbapi.com/?t=game+of+thrones&season="+ i +"&apikey=4e7d669c");
-			DadosDaTemporada dadosDaTemporada = conversor.obterDados(json, DadosDaTemporada.class);
-			temporadas.add(dadosDaTemporada);
-		}
-		
-		//Imprimindo temporadas.
-		temporadas.forEach(System.out::println);
-
+		Main main = new Main();
+		main.exibirMenu();
 	}
 
 }
-
-
-// https://www.omdbapi.com/?t=&apikey=4e7d669c
--- src/main/java/br/com/screengeek/main/Main.java
+++ src/main/java/br/com/screengeek/main/Main.java
@@ -1,8 +1,13 @@
 package br.com.screengeek.main;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
+import br.com.screengeek.model.DadosDaSerie;
+import br.com.screengeek.model.DadosDaTemporada;
 import br.com.screengeek.service.ConsumoApi;
+import br.com.screengeek.service.ConverteDados;
 
 //Classe Main feita para definir funções e métodos para interagir com o usuário.
 //Copiando funcionalidades da classe Application para deixar o código mais legível.
@@ -10,14 +15,31 @@ public class Main {
     
     //Constantes.
     Scanner scanner = new Scanner(System.in);
-    private final ConsumoApi CONSUMO_DE_API = new ConsumoApi();
     private final String URL = "https://www.omdbapi.com/?t=";
-    private final String API_KEY = "4e7d669c";
+    private final String API_KEY = "&apikey=4e7d669c";
+    private ConsumoApi consumoDeApi = new ConsumoApi();
+    private ConverteDados conversor = new ConverteDados();     //Consegue converter a API para um objeto instanciado.
 
     public void exibirMenu(){
         System.out.println("Digite o nome da série que deseja buscar: ");
         var nomeSerie = scanner.nextLine();
-		var json = CONSUMO_DE_API.obterDados(URL + nomeSerie.replace(" ", "+") + API_KEY);
+		var json = consumoDeApi.obterDados(URL + nomeSerie.replace(" ", "+") + API_KEY);
+        //Prcesso abaixo feito para puxar as informações da API e mostrar na tela, serve também para a classe DadosDoEpisodio. Toda vez que usar o conversor, precisa atualizar a variável JSON.
+		DadosDaSerie dados = conversor.obterDados(json, DadosDaSerie.class); //Fazendo a conversão do Json e atribuindo para uma variável, nesse caso uma classe DadosDaSerie.
+		System.out.println("\n\nSérie --->  " + dados);
 
+        //Lista vai ser usada para armazenar todas as instâncias, para depois imprimir a mesma.
+		List<DadosDaTemporada> temporadas = new ArrayList<>();
+		//Mesmo processo, mas o primeiro for serve para criar um objeto para cada uma das temporadas e armazenar no array 'temporadas'. Para saber quantas vezes iterar (número de temporadas da serie), apenas buscar o atributo temporadas da classe DadosDaSerie instanciada anteriormente. 
+		//A cada iteração, é necessário alterar o link.
+		System.out.println("\n\nInformações de todas as temporadas da série: \n");
+		for (int i = 1; i <= dados.temporadas(); i++) {
+			json = consumoDeApi.obterDados(URL + nomeSerie.replace(" ", "+") + "&season=" + i + API_KEY);
+			DadosDaTemporada dadosDaTemporada = conversor.obterDados(json, DadosDaTemporada.class);
+			temporadas.add(dadosDaTemporada);
+		}
+
+        //Imprimindo temporadas.
+		temporadas.forEach(System.out::println);
     }
 }
